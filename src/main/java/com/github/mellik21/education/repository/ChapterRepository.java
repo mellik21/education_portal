6c6c8fbf96d9bf90c3f61cc691928d7e7dd88813
Vladimir Shefer <quameu@gmail.com>
2021-07-23 22:18:06 +0300

    Add spring-test dependency

--- src/main/java/com/github/mellik21/education/repository/ChapterRepository.java
+++ src/main/java/com/github/mellik21/education/repository/ChapterRepository.java
@@ -2,9 +2,7 @@ package com.github.mellik21.education.repository;
 
 import com.github.mellik21.education.model.Chapter;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ChapterRepository extends CrudRepository<Chapter, String> {
 
 }
--- src/main/java/com/github/mellik21/education/repository/CourseRepository.java
+++ src/main/java/com/github/mellik21/education/repository/CourseRepository.java
@@ -1,17 +1,11 @@
 package com.github.mellik21.education.repository;
 
 import com.github.mellik21.education.model.Course;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface CourseRepository extends CrudRepository<Course, String> {
 
-    @Query("")
-    List<Course> search(@Param("searchQuery") String searchQuery);
+//    @Query("SELECT c FROM Course c WHERE c.title=:searchQuery OR c.description=:searchQuery")
+//    List<Course> search(@Param("searchQuery") String searchQuery);
 
 }
