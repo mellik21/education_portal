8bb7c850705c20808cf976b6f9bc7731ce2778d3
Vladimir Shefer <quameu@gmail.com>
2021-07-23 22:26:24 +0300

    Fix CourseRepository

--- src/main/java/com/github/mellik21/education/repository/CourseRepository.java
+++ src/main/java/com/github/mellik21/education/repository/CourseRepository.java
@@ -1,11 +1,15 @@
 package com.github.mellik21.education.repository;
 
 import com.github.mellik21.education.model.Course;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, String> {
 
-//    @Query("SELECT c FROM Course c WHERE c.title=:searchQuery OR c.description=:searchQuery")
-//    List<Course> search(@Param("searchQuery") String searchQuery);
+    @Query("SELECT c FROM Course c WHERE c.title=:searchQuery OR c.description=:searchQuery")
+    List<Course> search(@Param("searchQuery") String searchQuery);
 
 }
